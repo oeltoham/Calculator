264b791757a2a8fa1565562cd29234e1b3122971
oeltoham <oeltoham@cpcom102macm45.care.asu.edu>
2017-09-12 19:36:10 -0700

    v69

--- Omar_Eltohamy_Calculator_Project1/ViewController.swift
+++ Omar_Eltohamy_Calculator_Project1/ViewController.swift
@@ -9,7 +9,99 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var CalcScreenNumb:Double = 0;
+    var CalcPrev:Double=0;
+    var CalcMathPerf = false
+    var CalcOper = 0;
+    @IBOutlet weak var CalcLabel: UILabel!
+    
+    @IBAction func CalcNumbers(_ sender: UIButton)
+    {
+        if CalcMathPerf == true {
+            if sender.tag == 17 {
+                self.CalcLabel.text = String(".")
+            }
+            else
+            {self.CalcLabel.text = String(sender.tag-1)
+            self.CalcScreenNumb = Double(self.CalcLabel.text!)!
+            self.CalcMathPerf = false
+            }}
+        else
+        {
+            if sender.tag == 17 {
+                self.CalcLabel.text = String(".")
+            }
+            else{
+            self.CalcLabel.text = self.CalcLabel.text! + String(sender.tag-1)
+            self.CalcScreenNumb = Double(self.CalcLabel.text!)!
+            }}
+       
+    }
 
+    
+    @IBAction func CalcOthers(_ sender: UIButton)
+    {
+        if CalcLabel.text != "" && sender.tag != 16
+        {
+            CalcPrev = Double(CalcLabel.text!)!
+            if sender.tag == 11
+            {
+                CalcLabel.text = ""
+                CalcPrev = 0
+                CalcScreenNumb = 0
+                CalcOper = 0}
+            else if sender.tag == 12 //Division
+            {
+                CalcLabel.text = "/";
+            }
+            else if sender.tag == 13 //Multiplication
+            {
+                CalcLabel.text = "*";
+            }
+            else if sender.tag == 14 //Subtraction
+            {
+                CalcLabel.text = "-";
+            }
+            else if sender.tag == 15 //Addition
+            {
+                CalcLabel.text = "+";
+            }
+            else if sender.tag == 18
+            {
+                CalcLabel.text = "+/-";
+            }
+            CalcOper = sender.tag
+            CalcMathPerf = true;
+        }
+        else if sender.tag == 16
+        {
+        
+            if self.CalcOper == 12
+            {
+                CalcLabel.text = String(CalcPrev / CalcScreenNumb)
+            }
+            else if self.CalcOper == 13
+            {
+                CalcLabel.text = String(CalcPrev * CalcScreenNumb)
+            }
+            else if self.CalcOper == 14
+            {
+                CalcLabel.text = String(CalcPrev - CalcScreenNumb)
+            }
+            else if self.CalcOper == 15
+            {
+                CalcLabel.text = String(CalcPrev + CalcScreenNumb)
+            }
+            else if self.CalcOper == 18
+            {
+                CalcScreenNumb = CalcScreenNumb * -1;
+                CalcLabel.text = "\(CalcScreenNumb)"
+            }
+     
+    
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +113,5 @@ class ViewController: UIViewController {
     }
 
 
-}
 
+    }
